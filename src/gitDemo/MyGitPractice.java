3a5e696ce3c82fedc883d28647ab452cd216d466
lavan <lavan@DESKTOP-6TTKAI2>
2022-02-12 20:20:18 -0800

    First Commit

---
+++ src/gitDemo/MyGitPractice.java
@@ -0,0 +1,17 @@
+package gitDemo;
+
+public class MyGitPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String H = "y";
+		if (H=="y") {
+			System.out.println("Means yes");
+		}
+		else	{
+			System.out.println("Means No");
+		}
+
+	}
+
+}
