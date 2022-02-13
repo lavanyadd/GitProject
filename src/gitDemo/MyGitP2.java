71fc799a75a3b32cc186d6f3b3b9172302ece8ab
lavanyadd <dlavanyadasari@gmail.com>
2022-02-13 15:57:37 -0800

    Commiting to new branch2

--- src/gitDemo/MyGitP2.java
+++ src/gitDemo/MyGitP2.java
@@ -5,6 +5,7 @@ public class MyGitP2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is new class");
+		System.out.println("modifying the class");
 
 	}
 
