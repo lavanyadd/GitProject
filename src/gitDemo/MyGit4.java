7e71cf6f79c67a8cf96cf904ac0ea0441ec18d3f
lavanyadd <dlavanyadasari@gmail.com>
2022-02-13 16:42:08 -0800

    Another class is added

---
+++ src/gitDemo/MyGit4.java
@@ -0,0 +1,15 @@
+package gitDemo;
+
+public class MyGit4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 4; int num2 = 8;
+		if ((num1+num2)%2==0) {
+			System.out.println("even");
+		}else {
+			System.out.println("ODD   ....");
+		}
+	}
+
+}
